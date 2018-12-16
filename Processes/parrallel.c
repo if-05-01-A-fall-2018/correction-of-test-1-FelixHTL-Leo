479ef1b2364ae06473ff08eadf9f677d62d71d87
FelixHTL-Leo <felixbogi@gmail.com>
2018-12-16 16:28:26 +0100

    Komplett Fertig. Parallel habe ich auf der vm gemacht da ich sonst es nicht compilieren konnte. Doch auf der vm hat das abgeben nicht funktioniert und daher habe ich es nochmal geschrieben normal geschrieben, kann es jedoch nicht compilieren und somit nicht checken ob ich mich wo vertippt habe. Auf der vm haben alle befehle funktioniert.

--- Processes/parrallel.c
+++ Processes/parrallel.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h> //added include
 
+char* input[] = ["ls","df","ps", "pwd"];
+//deleted unessacary char
 int main(int argc, char const *argv[]) {
-  char input = ["ls","df","ps", "pwd"];
-  char* save;
-  int pid = fork();
-  if(pid == 0){
-      exevp(input[0], input);
+  int i = 0;
+  while(i < 4){ //added while
+    pid_t pid = fork();
+    if(pid == 0){
+      char* args[2] = {input[i], 0}; //added args
+      execvp(args[0], args); // changed from execvp(input[0], input)
+
+      printf("fail");
+    }
+    else{
+      wait(0); //added else with wait
+    }
+    i++;
   }
   printf("done");
   return 0;
